807cff8061cbbb8d4736b527c4cfd5a1d12f7c3b
vasechkin <vasechkin@gmail.com>
2026-04-25 00:04:13 +0300

    feat: download todos

--- static/scripts.js
+++ static/scripts.js
@@ -1,21 +1,89 @@
-document.addEventListener('DOMContentLoaded', () => {
-    let btnShowAll = document.getElementById('show_all');
-    if (btnShowAll){
-        
-        btnShowAll.addEventListener('click', () => {
-            window.location.href = '/'
-        });
+function escapeHtml(value) {
+    let str = '';
+    if (value === null || value === undefined) {
+        str = '';
+    }else {
+        str = String(value)
     }
-    let btnFilter = document.getElementById('filter');
-    if (btnFilter){
 
-        btnFilter.addEventListener('click', () => {
-            const params = new URLSearchParams();
-            params.set('limit', 100);
-            params.set('skip', 0);
-            params.set('is_completed', 'false');
-            window.location.href = '/?' + params.toString();
+    return str
+        .replace(/&/g, '&amp;')
+        .replace(/</g, '&lt;')
+        .replace(/>/g, '&gt;')
+        .replace(/"/g, '&quot;')
+        .replace(/'/g, '&#39;')
+        .replace(/`/g, '&#x60;');
+};
+document.addEventListener('DOMContentLoaded', () => {
+    
+    let todosContainer = document.getElementById('todos_container');
+    let btnFilter = document.getElementById('filter');
+    let btnShowAll = document.getElementById('show_all');
+
+    async function downloadTodos(params = {}) {
+        const url = new URL('/todo', window.location.origin);
+
+        todosContainer.innerHTML = '<p>download</p>';
+
+
+        try {
+            const url = new URL('/todo', window.location.origin);
+
+            for (let [key, value] of Object.entries(params)) { // если парам пуст, то ничего не передаем 
+                url.searchParams.append(key, value);
+            }
+
+            let response = await fetch(url);
+
+            if (!response.ok) {
+                todosContainer.innerHTML = `<p>error: ${response.status}</p>`;
+            }
+
+            let todos = await response.json();
+
+            displayTodos(todos);
+        } catch (error) {
+            todosContainer.innerHTML = `<p>error: ${error.message}</p>`;
+        }
+    }
+
+    function displayTodos(todos) {
+        if (todos.length === 0) {
+            todosContainer.innerHTML = '<p>no(</p>';
+            return;
+        }
+        let ulContainerTodos = document.createElement('ul');
+
+        todos.forEach(todo => {
+            let li = document.createElement('li');
+
+            li.innerHTML = `
+            <p>title: ${escapeHtml(todo.title)}</p>
+            <p>description: ${escapeHtml(todo.description)}</p>
+            <p>is_completed: ${escapeHtml(todo.is_completed)}</p>
+            `;
+
+            ulContainerTodos.appendChild(li);
+        });
+
+        todosContainer.textContent = '';
+        todosContainer.appendChild(ulContainerTodos);
+
+    }
+    if (btnShowAll) {
+        btnShowAll.addEventListener('click', async () => {
+            await downloadTodos()
         });
     }
+
+    if (btnFilter) {
+        btnFilter.addEventListener('click', async () => {
+            await downloadTodos({
+                limit: 10,
+                skip: 0,
+                is_completed: false
+            }
+            );
+        });
     }
-);
+});
