21cce57e63bd5da14606a927282434a45e0ceeec
vasechkin <vasechkin@gmail.com>
2026-05-05 00:41:19 +0300

    feat: field_validator

--- schemas.py
+++ schemas.py
@@ -2,7 +2,8 @@ from datetime import datetime
 from pydantic import (
     BaseModel,
     Field,
-    ConfigDict
+    ConfigDict,
+    field_validator
 )
 
 MAX_LENGTH = 256
@@ -14,11 +15,25 @@ class ToDoCreate(BaseModel):
     is_completed: bool = False
     reminder_at: datetime | None = None
 
+    @field_validator('reminder_at')
+    @classmethod
+    def validate_reminder(cls, reminder: datetime | None) -> datetime | None:
+        if reminder is not None and reminder < datetime.now():
+            raise ValueError('напоминание можно поставить только на будущее')
+        return reminder
+
 class ToDoUpdate(BaseModel):
     title: str | None = Field(None, min_length=MIN_LENGTH, max_length=MAX_LENGTH)
     description: str | None = Field(None, min_length=MIN_LENGTH, max_length=MAX_LENGTH)
     is_completed: bool | None = None
     reminder_at: datetime | None = None
+
+    @field_validator('reminder_at')
+    @classmethod
+    def validate_reminder(cls, reminder: datetime | None) -> datetime | None:
+        if reminder is not None and reminder < datetime.now():
+            raise ValueError('напоминание можно поставить только на будущее')
+        return reminder
 
 class ReminderSet(BaseModel):
     reminder_at: datetime
